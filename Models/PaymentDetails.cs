11dfb09dad022762954f113af03986407010d557
Ushakiran Vaddepalli <53057473+Ushakiran06@users.noreply.github.com>
2021-12-03 10:15:17 +0530

    Add files via upload

---
+++ Models/PaymentDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetails
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        //[Key]
+        public Guid PaymentDetailId { get; set; }
+
+        [Column(TypeName="nvarchar(100)")]
+        public string CradOwnerName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CradNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
+    }
+}
