d17857948b5afde9b20ea3dc47d38d2ba615e9fe
Ushakiran Vaddepalli <53057473+Ushakiran06@users.noreply.github.com>
2021-12-03 10:14:13 +0530

    Add files via upload

---
+++ Migration/20211130101731_PaymentsDetail.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class PaymentsDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PaymentDetails",
+                table: "PaymentDetails");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PaymentDetailId",
+                table: "PaymentDetails",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "PaymentDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PaymentDetails",
+                table: "PaymentDetails",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PaymentDetails",
+                table: "PaymentDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "PaymentDetails");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PaymentDetailId",
+                table: "PaymentDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PaymentDetails",
+                table: "PaymentDetails",
+                column: "PaymentDetailId");
+        }
+    }
+}
